8dae1e3af74895a6759d7e8b97646e335951eed3
gggaa2152 <60614405+gggaa2152@users.noreply.github.com>
2026-02-13 18:34:20 +0800

    Add Android.mk

---
+++ jni/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := PureImGuiElf
+
+LOCAL_CFLAGS   := -std=c11 -Wall
+LOCAL_CPPFLAGS := -std=c++17 -Wall -fexceptions -frtti
+LOCAL_CPPFLAGS += -DIMGUI_IMPL_OPENGL_ES3
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/imgui
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/backends
+
+LOCAL_SRC_FILES := main.cpp
+LOCAL_SRC_FILES += imgui/imgui.cpp
+LOCAL_SRC_FILES += imgui/imgui_draw.cpp
+LOCAL_SRC_FILES += imgui/imgui_tables.cpp
+LOCAL_SRC_FILES += imgui/imgui_widgets.cpp
+LOCAL_SRC_FILES += imgui/imgui_demo.cpp
+LOCAL_SRC_FILES += backends/imgui_impl_android.cpp
+LOCAL_SRC_FILES += backends/imgui_impl_opengl3.cpp
+
+LOCAL_LDLIBS := -landroid -lEGL -lGLESv3 -llog
+
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
+include $(BUILD_EXECUTABLE)
