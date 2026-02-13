d2f1e94e50fe01373557e309ab34fefc8dc59714
gggaa2152 <60614405+gggaa2152@users.noreply.github.com>
2026-02-13 19:47:28 +0800

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -10,6 +10,7 @@ LOCAL_CPPFLAGS += -DIMGUI_IMPL_OPENGL_ES3
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/imgui
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/backends
+LOCAL_C_INCLUDES += $(NDK_ROOT)/sources/android/native_app_glue
 
 LOCAL_SRC_FILES := main.cpp
 LOCAL_SRC_FILES += imgui/imgui.cpp
@@ -20,8 +21,12 @@ LOCAL_SRC_FILES += imgui/imgui_demo.cpp
 LOCAL_SRC_FILES += backends/imgui_impl_android.cpp
 LOCAL_SRC_FILES += backends/imgui_impl_opengl3.cpp
 
+# ⚠️ 直接把 native_app_glue.c 当作源文件编译
+LOCAL_SRC_FILES += $(NDK_ROOT)/sources/android/native_app_glue/android_native_app_glue.c
+
 LOCAL_LDLIBS := -landroid -lEGL -lGLESv3 -llog
 
-LOCAL_STATIC_LIBRARIES := android_native_app_glue
+# ❌ 不再需要 LOCAL_STATIC_LIBRARIES
+# ✅ 也不需要 $(call import-module, ...)
 
 include $(BUILD_EXECUTABLE)
